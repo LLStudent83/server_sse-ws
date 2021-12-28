a2c72be98fbaa5fe3308ab518f99abe9dd6c99a5
Dmitry Prigulnov <prigulnov@mail.ru>
2021-12-28 15:10:02 +0500

    исправил ошибки

--- sign/sign.js
+++ sign/sign.js
@@ -2,10 +2,6 @@ const signApi = {
   users: [],
 
   contains(userName) {
-    // for (const user in this.users) {
-    //   if (this.users[user] === userName) return false;
-    // }
-    // return true;
     return this.users.some((item) => item === userName);
   },
 
@@ -14,15 +10,11 @@ const signApi = {
   },
 
   add(user) {
-    // this.users[id] = user;
     this.users.push(user);
     return this.users;
   },
 
   remove(userName) {
-    // for (const user in this.users) {
-    //   if (this.users[user] === userName) delete this.users[user];
-    // }
     this.serialNumber = this.users.findIndex((item) => item === userName);
     this.users.splice(this.serialNumber, 1);
     return this.users;
